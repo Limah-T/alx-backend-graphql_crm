6a025de700b4d5f6a4e5cf93b32b62ded2f58d5b
Limah-T <limahtechnology@yahoo.com>
2025-08-19 12:49:27 +0100

    ...

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -21,3 +21,14 @@ class Query(graphene.ObjectType):
 
 
 schema = graphene.Schema(query=Query)
+
+# import graphene
+# from crm.schema import Query as CRMQuery, Mutation as CRMMutation
+
+# class Query(CRMQuery, graphene.ObjectType):
+#     pass
+
+# class Mutation(CRMMutation, graphene.ObjectType):
+#     pass
+
+# schema = graphene.Schema(query=Query, mutation=Mutation)
